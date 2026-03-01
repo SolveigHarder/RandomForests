da88e63b8174d5fbf96e14a1dd11a1969ee8245b
Annika Lehmann <lannika20@gmail.com>
2026-03-01 11:42:10 +0100

    better?

--- R/Bagging_6_25.r
+++ R/Bagging_6_25.r
@@ -135,10 +135,18 @@ print.bagging_regression <- function(x, ...) {
 
 # TEST VON SOLVEIG
 set.seed(4)
-n <- 60
+n <- 100
+
+x <- sort(runif(n, -1, 1))
+y <- ifelse(x < -0.2, 1,
+            ifelse(x < 0.4, 3, 0)) + rnorm(n, sd = 0.2)
+
+X <- data.frame(x = x)
+
 id <- sample.int(n)
 train <- id[1:round(0.7*n)]
 test  <- id[(round(0.7*n)+1):n]
+
 
 fit <- fit_greedy_cart_regression(X[train, , drop=FALSE], y[train],
                                    max_splits = 5, min_leaf_size = 5)
